76c40d09d5e39da1b6b659b406eb74262e934c33
Piotr <piotrwydmuch95@gmail.com>
2021-06-30 18:24:03 +0200

    js callbacks works: app runing image processing after button event

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"net/http"
 	"strconv"
+	"sync"
 	"syscall/js"
 
 	"github.com/anthonynsimon/bild/imgio"
@@ -20,70 +21,54 @@ import (
 
 func main() {
 
-	
+	//Loading and decoding sample image
 	resp, err := http.Get("lenna.png")
-	fmt.Println(err)
-	
-	//type- OriginalImgRGBA =  *image.RGBA
+	fmt.Println(err)	
 	OriginalImgRGBA, err := png.Decode(resp.Body)
 	if err != nil {
 		log.Fatalf("Error decoding PNG: %s", err.Error())
 	}
-
-	// Create grayscale of img
-	newImgRGBA := Gaussian(OriginalImgRGBA, 9.0)
-	//fmt.Println(newImgRGBA)
-
-	buf := new(bytes.Buffer)
-	encoder := imgio.PNGEncoder()
-	encoder(buf, newImgRGBA)
-	newBitmap := buf.Bytes()
-	//fmt.Println("buf bytes: ", newBitmap)
-
-	// Type of new bitmap in Go- []uint8
-	// fmt.Println(reflect.TypeOf(newBitmap));
-
-	console := js.Global().Get("console")
-
-	dst := js.Global().Get("Uint8Array").New(len(newBitmap))
-	n := js.CopyBytesToJS(dst, newBitmap)
-	console.Call("log", "bytes copied:", strconv.Itoa(n))
-	js.Global().Call("displayImage", dst)
-
-}
-
-func Grayscale(img image.Image) *image.RGBA {
-	return GrayscaleWithWeights(img, 0.3, 0.6, 0.1)
-}
-
-func GrayscaleWithWeights(img image.Image, r, g, b float64) *image.RGBA {
-	src := clone.AsRGBA(img)
-	bounds := src.Bounds()
-	srcW, srcH := bounds.Dx(), bounds.Dy()
-
-	if bounds.Empty() {
-		return &image.RGBA{}
-	}
-
-	dst := image.NewRGBA(bounds)
-
-	parallel.Line(srcH, func(start, end int) {
-		for y := start; y < end; y++ {
-			for x := 0; x < srcW; x++ {
-				pos := y*src.Stride + x*4
-
-				c := r*float64(src.Pix[pos+0]) + g*float64(src.Pix[pos+1]) + b*float64(src.Pix[pos+2])
-				k := uint8(c + 0.5)
-				dst.Pix[pos] = k
-				dst.Pix[pos+1] = k
-				dst.Pix[pos+2] = k
-				dst.Pix[pos+3] = src.Pix[pos+3]
-			}
-		}
-	})
-	return dst
-}
 	
+	//JavaScript func declaratrion - calling from DOM is possible now
+	var cb js.Func
+	cb = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		EditPhoto(OriginalImgRGBA)
+		//cb.Release() // release the function if the button will not be clicked again
+		return nil
+	})
+	js.Global().Get("document").Call("getElementById", "myButton").Call("addEventListener", "click", cb)
+	
+	//App is ready for user actions
+	fmt.Println("Rdy")	
+	
+	//To wait for multiple goroutines to finish, we can use a wait group
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
+
+func EditPhoto(img image.Image) {
+		// Create grayscale of img
+		newImgRGBA := Gaussian(img, 9.0)
+		//fmt.Println(newImgRGBA)
+	
+		buf := new(bytes.Buffer)
+		encoder := imgio.PNGEncoder()
+		encoder(buf, newImgRGBA)
+		newBitmap := buf.Bytes()
+		//fmt.Println("buf bytes: ", newBitmap)
+	
+		// Type of new bitmap in Go- []uint8
+		// fmt.Println(reflect.TypeOf(newBitmap));
+	
+		console := js.Global().Get("console")
+	
+		dst := js.Global().Get("Uint8Array").New(len(newBitmap))
+		n := js.CopyBytesToJS(dst, newBitmap)
+		console.Call("log", "bytes copied:", strconv.Itoa(n))
+		js.Global().Call("displayImage", dst)
+}
+
 func Gaussian(src image.Image, radius float64) *image.RGBA {
 	if radius <= 0 {
 		return clone.AsRGBA(src)
@@ -104,3 +89,36 @@ func Gaussian(src image.Image, radius float64) *image.RGBA {
 
 	return result
 }
+
+func GrayscaleWithWeights(img image.Image, r, g, b float64) *image.RGBA {
+	src := clone.AsRGBA(img)
+	bounds := src.Bounds()
+	srcW, srcH := bounds.Dx(), bounds.Dy()
+	
+	if bounds.Empty() {
+		return &image.RGBA{}
+	}
+	
+	dst := image.NewRGBA(bounds)
+	
+	parallel.Line(srcH, func(start, end int) {
+		for y := start; y < end; y++ {
+			for x := 0; x < srcW; x++ {
+				pos := y*src.Stride + x*4
+				
+				c := r*float64(src.Pix[pos+0]) + g*float64(src.Pix[pos+1]) + b*float64(src.Pix[pos+2])
+				k := uint8(c + 0.5)
+				dst.Pix[pos] = k
+				dst.Pix[pos+1] = k
+				dst.Pix[pos+2] = k
+				dst.Pix[pos+3] = src.Pix[pos+3]
+			}
+		}
+	})
+	return dst
+}
+
+func Grayscale(img image.Image) *image.RGBA {
+	return GrayscaleWithWeights(img, 0.3, 0.6, 0.1)
+}
+
