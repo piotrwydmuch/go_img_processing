2059b962aa2893ea191b3f51c3299541bc969852
Piotr <piotrwydmuch95@gmail.com>
2021-11-03 07:51:16 +0100

    some styles added

--- pureJsImageProcessing.js
+++ pureJsImageProcessing.js
@@ -5,5 +5,8 @@ let sourceImage = document.getElementById('source-img');
 let pureJsCallBtnBlur = document.getElementById('pure-js-button')
 
 pureJsCallBtnBlur.addEventListener("click", () => {
+  let startTime = performance.now();
   StackBlur.image(sourceImage, transformedImage, 10, true);
+  let endTime = performance.now();
+  console.log(`%c pure js done. t: ${(endTime - startTime)}ms`, 'color: #dedede')
 })
