81e19de4f954cc1fc0ecce29100b59f3e11925b5
Piotr <piotrwydmuch95@gmail.com>
2021-11-15 18:06:21 +0100

    10x testing done - need backend flask server for work here

---
+++ src/pureJsImageProcessing.js
@@ -0,0 +1,12 @@
+import { image } from '/src/stackblur.js';
+
+let transformedImage = document.getElementById('transformed-image-js');
+let sourceImage = document.getElementById('source-img');
+let pureJsCallBtnBlur = document.getElementById('pure-js-button')
+
+pureJsCallBtnBlur.addEventListener("click", () => {
+  let startTime = performance.now();
+  image(sourceImage, transformedImage, 10, true);
+  let endTime = performance.now();
+  console.log(`%c pure js done. t: ${(endTime - startTime)}ms`, 'color: #dedede')
+})
